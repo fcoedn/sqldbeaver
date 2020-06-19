458f1b88a08a5e7197082d13fc87a48e59b8f103
Francisco Filho <francisco.filho@armazemparaiba.com.br>
2020-06-19 10:25:09 -0300

    initial-2

---
+++ General/Scripts/Script-12.sql
@@ -0,0 +1,14 @@
+-- public.numeracon definition
+
+-- Drop table Francisco Filho
+
+-- DROP TABLE public.numeracon;
+
+CREATE TABLE public.numeracon (
+	sg_loja varchar(3) NULL,
+	tp_contrato numeric(2) NULL,
+	tp_fin numeric(2) NULL,
+	nr_contrato numeric(8) NULL,
+	cd_chave varchar(7) NOT NULL,
+	CONSTRAINT numeracon_pkey PRIMARY KEY (cd_chave)
+);
